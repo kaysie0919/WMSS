f56b7e92790bc0041a85408b3f71fc379d3b17b1
Kaysie Amador <kaysiekane321@gmail.com>
2025-05-06 20:39:49 +0800

    commit

--- login.php
+++ login.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/signin.css" rel="stylesheet">
     <style>
+      /* Optional: retain custom checkbox if necessary */
       .custom-checkbox {
         display: flex;
         align-items: center;
+        margin-top: 0.5rem;
       }
       .custom-checkbox input[type="checkbox"] {
         display: none;
@@ -37,60 +41,63 @@
         width: 10px;
         height: 10px;
         background: #007bff;
-        border-radius:100px;
+        border-radius: 100px;
       }
     </style>
   </head>
-  <body class="text-center">
-    <form id="myform" class="form-signin" action="test.php" method="post">
-      <h1 class="h3 mb-3 font-weight-normal">LogIn</h1>
-      <label for="inputEmail" class="sr-only">User Name</label>
-      <input type="text" name="username" class="form-control" placeholder="User Name" required autofocus>
-      <br>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <div class="custom-checkbox">
-        <input type="checkbox" id="showPassword">
-        <label for="showPassword">Show Password</label>
-      </div>
-      <br>
+  <body class="bg-dark">
+    <div class="container min-vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div class="w-100" style="max-width: 400px;">
+        <form id="myform" class="border rounded p-4 shadow-sm bg-gray" action="test.php" method="post">
+          <h2 class="mb-4 text-center ">Log In</h2>
 
-      <br><br>
-      <button id="saveForm" class="btn btn-lg btn-primary btn-block" type="button">LogIn</button>
-      <br>
-      <p>Create Account <a href="register.php">Sign up!</a></p>
-      <a href="forgetpass.php">Forgot Password?</a>
-      <br>
-      <a href="index.php" class="btn btn-secondary btn-block">Go Back Home</a>
-    </form>
-    <center><span id="result"></span></center>
-    
+          <input type="text" name="username" class="form-control mb-3" placeholder="User Name" required autofocus>
+
+          <input type="password" name="password" id="inputPassword" class="form-control mb-2" placeholder="Password" required>
+
+          <div class="custom-checkbox mb-3">
+            <input type="checkbox" id="showPassword">
+            <label for="showPassword">Show Password</label>
+          </div>
+
+          <button id="saveForm" class="btn btn-primary btn-block w-100 mb-3" type="button">Log In</button>
+
+          <p class="text-center mb-3"><a href="register.php">Sign up!</a></p>
+          <p class="text-center mb-3"><a href="forgetpass.php">Forgot Password?</a></p>
+
+          <a href="index.php" class="btn btn-secondary btn-block w-100">Go Back Home</a>
+        </form>
+
+        <div class="text-center mt-3">
+          <span id="result"></span>
+        </div>
+      </div>
+    </div>
+
     <script src="js/jquery-2.2.3.min.js"></script>
     <script>
-      $("#saveForm").click(function(){
-        $.post($("#myform").attr("action"), $("#myform :input").serializeArray(), function(info){ 
-          $("#result").html(info); 
+      $("#saveForm").click(function () {
+        $.post($("#myform").attr("action"), $("#myform :input").serializeArray(), function (info) {
+          $("#result").html(info);
         });
         clearInput();
       });
 
-      $("#myform").submit(function(){
+      $("#myform").submit(function () {
         return false;
       });
 
-      function clearInput(){
-        $("#myform :input").each(function() {
-          $(this).val('');
+      function clearInput() {
+        $("#myform :input").each(function () {
+          if (this.type !== "button" && this.type !== "submit" && this.type !== "reset") {
+            $(this).val('');
+          }
         });
       }
 
-      document.getElementById('showPassword').addEventListener('change', function() {
+      document.getElementById('showPassword').addEventListener('change', function () {
         var passwordInput = document.getElementById('inputPassword');
-        if (this.checked) {
-          passwordInput.type = 'text';
-        } else {
-          passwordInput.type = 'password';
-        }
+        passwordInput.type = this.checked ? 'text' : 'password';
       });
     </script>
   </body>
